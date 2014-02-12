bdb2290ca1090f3425d9bab63690c208b3ac748b
Norman Jaffe <turing@shaw.ca>
2014-02-12 15:40:55 -0800

    Removed generated files.

--- ExampleClient/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/M_M/Documents/YarpPlusPlus/ExampleClient
-# Build directory: /Users/M_M/Documents/YarpPlusPlus/ExampleClient
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ExampleService/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/M_M/Documents/YarpPlusPlus/ExampleService
-# Build directory: /Users/M_M/Documents/YarpPlusPlus/ExampleService
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
