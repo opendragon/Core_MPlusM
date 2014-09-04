957bd3e54374507893b9c87abe92749842dc247d
Norman Jaffe <turing@shaw.ca>
2014-09-04 09:57:31 -0700

    Fixed minor typo.

--- MovementDb/MovementDbAdapter/M+MMovementDbAdapterMain.cpp
+++ MovementDb/MovementDbAdapter/M+MMovementDbAdapterMain.cpp
@@ -54,7 +54,7 @@
  
  @brief The main application for the movement database adapter. */
 
-/*! @dir MovementAltAdapter
+/*! @dir MovementDbAdapter
  @brief The set of files that implement the movement database adapter. */
 #if defined(__APPLE__)
 # pragma clang diagnostic pop
