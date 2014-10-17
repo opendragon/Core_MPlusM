1d78a2c988603777428348c1de757c00f01afa49
Norman Jaffe <turing@shaw.ca>
2014-10-17 07:42:25 -0700

    Minor change to correctly calculate the line count.

--- sloc.sh
+++ sloc.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 sloccount ClientList CommonTests Exemplars KinectV2 LeapMotion MovementDb ParserTest PortLister RegistryService RequestCounter RequestInfo ServiceLister Version examples mpm odl
+echo ---------------
+echo subtract the following -
+echo ---------------
+sloccount KinectV2/KinectV2InputService/KinectLib LeapMotion/LeapMotionInputService/Leap*.h
