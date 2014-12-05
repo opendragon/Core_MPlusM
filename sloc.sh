63e7f6fa5f9c70d5cebca330409e018b4be357d2
Norman Jaffe <turing@shaw.ca>
2014-12-05 13:18:44 -0800

    Added support for generating Python bindings via SWIG.

--- sloc.sh
+++ sloc.sh
@@ -4,4 +4,4 @@ sloccount ClientList CommonTests Exemplars FindServices KinectV2 LeapMotion Move
 echo ---------------
 echo subtract the following -
 echo ---------------
-sloccount KinectV2/KinectV2InputService/KinectLib LeapMotion/LeapMotionInputService/Leap*.h ViconDataStream/ViconDataStreamInputService/ViconLib mpm/swig_mpm_in mpm/swig_mpm_out odl/swig_odl_in odl/swig_odl_out
+sloccount KinectV2/KinectV2InputService/KinectLib LeapMotion/LeapMotionInputService/Leap*.h ViconDataStream/ViconDataStreamInputService/ViconLib mpm/swig_mpm_in mpm/swig_mpm_out_csharp mpm/swig_mpm_out_python odl/swig_odl_in odl/swig_odl_out_csharp odl/swig_odl_out_python
