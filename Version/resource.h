7732df71f44fde007423afeb51e091b10bae78b6
Norman Jaffe <turing@shaw.ca>
2015-04-15 07:49:46 -0700

    Fixed up the git file set.

--- Version/resource.h
+++ Version/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by mpmRequestCounterService.rc
+// Used by mpmVersion.rc
 
 // Next default values for new objects
 // 
