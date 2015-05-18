69dbedb59a7a9699496997cc7dc06f186ad439c7
Norman Jaffe <turing@shaw.ca>
2015-05-17 17:52:18 -0700

    Added support for parsing argument descriptions, switched services to using argument descriptions.

---
+++ mpm/M+MAddressArgumentDescriptor.cpp
@@ -0,0 +1,174 @@
+//--------------------------------------------------------------------------------------------------
+//
+//  File:       mpm/M+MFilePathArgumentDescriptor.cpp
+//
+//  Project:    M+M
+//
+//  Contains:   The class definition for the minimal functionality required to represent a
+//              filepath-type command-line argument.
+//
+//  Written by: Norman Jaffe
+//
+//  Copyright:  (c) 2015 by H Plus Technologies Ltd. and Simon Fraser University.
+//
+//              All rights reserved. Redistribution and use in source and binary forms, with or
+//              without modification, are permitted provided that the following conditions are met:
+//                * Redistributions of source code must retain the above copyright notice, this list
+//                  of conditions and the following disclaimer.
+//                * Redistributions in binary form must reproduce the above copyright notice, this
+//                  list of conditions and the following disclaimer in the documentation and / or
+//                  other materials provided with the distribution.
+//                * Neither the name of the copyright holders nor the names of its contributors may
+//                  be used to endorse or promote products derived from this software without
+//                  specific prior written permission.
+//
+//              THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+//              EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//              OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+//              SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//              INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+//              TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+//              BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//              CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//              ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+//              DAMAGE.
+//
+//  Created:    2015-05-15
+//
+//--------------------------------------------------------------------------------------------------
+
+#include <mpm/M+MFilePathArgumentDescriptor.h>
+
+//#include <odl/ODEnableLogging.h>
+#include <odl/ODLogging.h>
+
+#if defined(__APPLE__)
+# pragma clang diagnostic push
+# pragma clang diagnostic ignored "-Wunknown-pragmas"
+# pragma clang diagnostic ignored "-Wdocumentation-unknown-command"
+#endif // defined(__APPLE__)
+/*! @file
+ @brief The definition for the minimal functionality required to represent a filepath-type
+ command-line argument. */
+#if defined(__APPLE__)
+# pragma clang diagnostic pop
+#endif // defined(__APPLE__)
+
+#if defined(__APPLE__)
+# pragma mark Namespace references
+#endif // defined(__APPLE__)
+
+using namespace MplusM;
+using namespace MplusM::Common;
+using namespace MplusM::Utilities;
+
+#if defined(__APPLE__)
+# pragma mark Private structures, constants and variables
+#endif // defined(__APPLE__)
+
+#if defined(__APPLE__)
+# pragma mark Global constants and variables
+#endif // defined(__APPLE__)
+
+#if defined(__APPLE__)
+# pragma mark Local functions
+#endif // defined(__APPLE__)
+
+#if defined(__APPLE__)
+# pragma mark Class methods
+#endif // defined(__APPLE__)
+
+#if defined(__APPLE__)
+# pragma mark Constructors and Destructors
+#endif // defined(__APPLE__)
+
+FilePathArgumentDescriptor::FilePathArgumentDescriptor(const YarpString & argName,
+                                                       const YarpString & argDescription,
+                                                       const YarpString & defaultValue,
+                                                       const bool         isOptional,
+                                                       const bool         forOutput,
+                                                       YarpString *       argumentReference) :
+    inherited(argName, argDescription, defaultValue, isOptional, argumentReference),
+    _forOutput(forOutput)
+{
+    OD_LOG_ENTER(); //####
+    OD_LOG_S3s("argName = ", argName, "argDescription = ", argDescription, "defaultValue = ", //####
+               defaultValue); //####
+    OD_LOG_B2("isOptional = ", isOptional, "forOutput = ", forOutput); //####
+    OD_LOG_P1("argumentReference = ", argumentReference); //####
+    OD_LOG_EXIT_P(this); //####
+} // FilePathArgumentDescriptor::FilePathArgumentDescriptor
+
+FilePathArgumentDescriptor::~FilePathArgumentDescriptor(void)
+{
+    OD_LOG_OBJENTER(); //####
+    OD_LOG_OBJEXIT(); //####
+} // FilePathArgumentDescriptor::~FilePathArgumentDescriptor
+
+#if defined(__APPLE__)
+# pragma mark Actions and Accessors
+#endif // defined(__APPLE__)
+
+YarpString FilePathArgumentDescriptor::getProcessedValue(void)
+const
+{
+    OD_LOG_OBJENTER(); //####
+    YarpString result;
+
+    if (_argumentReference)
+    {
+        result = *_argumentReference;
+    }
+    else
+    {
+        result = _defaultValue;
+    }
+    OD_LOG_OBJEXIT_s(result); //####
+    return result;
+} // FilePathArgumentDescriptor::getProcessedValue
+
+Common::YarpString FilePathArgumentDescriptor::toString(void)
+const
+{
+    OD_LOG_OBJENTER(); //####
+    Common::YarpString result(isOptional() ? "f" : "F");
+    
+    result += (_forOutput ? "o" : "i");
+    result += standardFields();
+    OD_LOG_OBJEXIT_s(result); //####
+    return result;
+} // FilePathArgumentDescriptor::toString
+
+bool FilePathArgumentDescriptor::validate(const Common::YarpString & value)
+const
+{
+    OD_LOG_OBJENTER(); //####
+    bool result;
+    
+    if (_forOutput)
+    {
+#if MAC_OR_LINUX_
+        result = (0 == access(value.c_str(), W_OK));
+#else // ! MAC_OR_LINUX_
+        result = (0 == _access(value.c_str(), 2));
+#endif // ! MAC_OR_LINUX_
+    }
+    else
+    {
+#if MAC_OR_LINUX_
+        result = (0 == access(value.c_str(), R_OK));
+#else // ! MAC_OR_LINUX_
+        result = (0 == _access(value.c_str(), 4));
+#endif // ! MAC_OR_LINUX_
+    }
+    if (result && _argumentReference)
+    {
+        *_argumentReference = value;
+    }
+    OD_LOG_OBJEXIT_B(result); //####
+    return result;
+} // FilePathArgumentDescriptor::validate
+
+#if defined(__APPLE__)
+# pragma mark Global functions
+#endif // defined(__APPLE__)
---
+++ mpm/M+MAddressArgumentDescriptor.h
@@ -0,0 +1,134 @@
+//--------------------------------------------------------------------------------------------------
+//
+//  File:       mpm/M+MFilePathArgumentDescriptor.h
+//
+//  Project:    M+M
+//
+//  Contains:   The class declaration for the minimal functionality required to represent a
+//              filepath-type command-line argument.
+//
+//  Written by: Norman Jaffe
+//
+//  Copyright:  (c) 2015 by H Plus Technologies Ltd. and Simon Fraser University.
+//
+//              All rights reserved. Redistribution and use in source and binary forms, with or
+//              without modification, are permitted provided that the following conditions are met:
+//                * Redistributions of source code must retain the above copyright notice, this list
+//                  of conditions and the following disclaimer.
+//                * Redistributions in binary form must reproduce the above copyright notice, this
+//                  list of conditions and the following disclaimer in the documentation and / or
+//                  other materials provided with the distribution.
+//                * Neither the name of the copyright holders nor the names of its contributors may
+//                  be used to endorse or promote products derived from this software without
+//                  specific prior written permission.
+//
+//              THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+//              EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//              OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+//              SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//              INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+//              TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+//              BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//              CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//              ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+//              DAMAGE.
+//
+//  Created:    2015-05-15
+//
+//--------------------------------------------------------------------------------------------------
+
+#if (! defined(MpMFilePathArgumentDescriptor_H_))
+# define MpMFilePathArgumentDescriptor_H_ /* Header guard */
+
+# include <mpm/M+MStringArgumentDescriptor.h>
+
+# if defined(__APPLE__)
+#  pragma clang diagnostic push
+#  pragma clang diagnostic ignored "-Wunknown-pragmas"
+#  pragma clang diagnostic ignored "-Wdocumentation-unknown-command"
+# endif // defined(__APPLE__)
+/*! @file
+ @brief The class declaration for the minimal functionality required to represent a filepath-type
+ command-line argument. */
+# if defined(__APPLE__)
+#  pragma clang diagnostic pop
+# endif // defined(__APPLE__)
+
+namespace MplusM
+{
+    namespace Utilities
+    {
+        /*! @brief A filepath-type argument description. */
+        class FilePathArgumentDescriptor : public StringArgumentDescriptor
+        {
+        public :
+            
+            /*! @brief The constructor.
+             @param argName The name of the command-line argument.
+             @param argDescription A description of the command-line argument.
+             @param isOptional @c true if the argument is optional and @c false otherwise.
+             @param forOutput @c true if the file will be used for output and @c false otherwise.
+             @param argumentReference If non-@c NULL, the variable to be set with the argument
+             value. */
+            FilePathArgumentDescriptor(const Common::YarpString & argName,
+                                       const Common::YarpString & argDescription,
+                                       const Common::YarpString & defaultValue,
+                                       const bool                 isOptional,
+                                       const bool                 forOutput = false,
+                                       Common::YarpString *       argumentReference = NULL);
+            
+            /*! @brief The destructor. */
+            virtual ~FilePathArgumentDescriptor(void);
+            
+            /*! @brief Return the processed value.
+             @returns The processed value. */
+            virtual Common::YarpString getProcessedValue(void)
+            const;
+
+            /*! @brief Convert to a printable representation.
+             @returns A printable representation of the descriptor. */
+            virtual Common::YarpString toString(void)
+            const;
+            
+            /*! @brief Check an input value against the constraints of the descriptor.
+             @param value The value to be checked.
+             @returns @c true if the value is within the domain of the descriptor and @c false
+             otherwise. */
+            virtual bool validate(const Common::YarpString & value)
+            const;
+            
+        protected :
+        
+        private :
+            
+            COPY_AND_ASSIGNMENT_(FilePathArgumentDescriptor);
+            
+        public :
+        
+        protected :
+        
+        private :
+            
+            /*! @brief The class that this class is derived from. */
+            typedef StringArgumentDescriptor inherited;
+            
+            /*! @brief @c true if the file path will be used for output and @c false otherwise. */
+            bool _forOutput;
+            
+# if defined(__APPLE__)
+#  pragma clang diagnostic push
+#  pragma clang diagnostic ignored "-Wunused-private-field"
+# endif // defined(__APPLE__)
+            /*! @brief Filler to pad to alignment boundary */
+            char _filler[7];
+# if defined(__APPLE__)
+#  pragma clang diagnostic pop
+# endif // defined(__APPLE__)
+            
+        }; // FilePathArgumentDescriptor
+        
+    } // Utilities
+    
+} // MplusM
+
+#endif // ! defined(MpMFilePathArgumentDescriptor_H_)
