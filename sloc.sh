b0ce2ea046ae112634b37903978f0aa3a33d2a7f
Norman Jaffe <turing@shaw.ca>
2015-06-29 15:29:17 -0700

    Corrected a path reference.

--- sloc.sh
+++ sloc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sloccount Address Blob ClientList CommonTests Emotiv FindServices JavaScript KinectV2 LeapMotion MovementDb OrganicMotion ParserTest PortLister ProComp2 RegistryService RequestCounter RequestInfo ServiceLister ServiceMetrics StopService Tunnel Unreal Version ViconDataStream examples mpm odl
+sloccount Address Blob ClientList CommonTests Emotiv FindServices JavaScript KinectV2 LeapMotion MovementDb OpenStage ParserTest PortLister ProComp2 RegistryService RequestCounter RequestInfo ServiceLister ServiceMetrics StopService Tunnel Unreal Version ViconDataStream examples mpm odl
 echo ---------------
 echo subtract the following -
 echo ---------------
-sloccount KinectV2/KinectV2InputService/KinectLib LeapMotion/LeapMotionInputService/Leap*.h ViconDataStream/ViconDataStreamInputService/ViconLib mpm/swig_mpm_in mpm/swig_mpm_out_csharp mpm/swig_mpm_out_python odl/swig_odl_in odl/swig_odl_out_csharp odl/swig_odl_out_python ProComp2/ProComp2InputService/ProCompLib/inc
+sloccount KinectV2/KinectV2InputService/KinectLib LeapMotion/LeapMotionInputService/Leap*.h OpenStage/OpenStageInputService/glm ViconDataStream/ViconDataStreamInputService/ViconLib mpm/swig_mpm_in mpm/swig_mpm_out_csharp mpm/swig_mpm_out_python odl/swig_odl_in odl/swig_odl_out_csharp odl/swig_odl_out_python ProComp2/ProComp2InputService/ProCompLib/inc
