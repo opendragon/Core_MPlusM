87051af931d725bca98cb0b564c0a8efe3bea429
Norman Jaffe <turing@shaw.ca>
2015-06-29 07:15:37 -0700

    Added an 'uninstall' script.

---
+++ uninstallM+M.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function ask_yes_or_no() {
+    read -p "$1 ([y]es or [N]o): "
+    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+        y|yes) echo "yes" ;;
+        *)     echo "no" ;;
+    esac
+}
+
+echo "This script will remove the M+M installation from this system,"
+echo "except for a reference in the system PATH variable."
+if [[ "no" == $(ask_yes_or_no "Are you sure?") || \
+      "no" == $(ask_yes_or_no "Are you *really* sure?") ]]
+then
+    exit 0
+fi
+sudo rm -r /opt/M+M
+echo "M+M installation removed."
