cf717be7e1ab602809a020977bce645ae3b545aa
Norman Jaffe <turing@shaw.ca>
2015-07-14 13:26:19 -0700

    Corrected the calculation of source lines.

--- sloc.sh
+++ sloc.sh
@@ -4,4 +4,4 @@ sloccount Address Blob ClientList CommonTests Emotiv FindServices JavaScript Kin
 echo ---------------
 echo subtract the following -
 echo ---------------
-sloccount KinectV2/KinectV2InputService/KinectLib LeapMotion/LeapMotionInputService/Leap*.h OpenStage/OpenStageInputService/glm ViconDataStream/ViconDataStreamInputService/ViconLib mpm/swig_mpm_in mpm/swig_mpm_out_csharp mpm/swig_mpm_out_python odl/swig_odl_in odl/swig_odl_out_csharp odl/swig_odl_out_python ProComp2/ProComp2InputService/ProCompLib/inc
+sloccount KinectV2/KinectV2InputService/KinectLib LeapMotion/LeapMotionInputService/Leap*.h OpenStage/OpenStageInputService/glm OpenStage/OpenStageInputService/om_sdk ViconDataStream/ViconDataStreamInputService/ViconLib mpm/swig_mpm_in mpm/swig_mpm_out_csharp mpm/swig_mpm_out_python odl/swig_odl_in odl/swig_odl_out_csharp odl/swig_odl_out_python ProComp2/ProComp2InputService/ProCompLib/inc
