a6ec2fc0845cb4187939415aba8bb0e0a2f2751d
Norman Jaffe <turing@shaw.ca>
2015-07-30 08:49:56 -0700

    Corrected some minor path problems.

--- sloc.sh
+++ sloc.sh
@@ -4,4 +4,4 @@ sloccount Address Blob ClientList CommonTests Emotiv FindServices JavaScript Kin
 echo ---------------
 echo subtract the following -
 echo ---------------
-sloccount KinectV2/KinectV2InputService/KinectLib LeapMotion/LeapMotionInputService/Leap*.h NatNet/NatNetInputService/NatNetSDK OpenStage/OpenStageInputService/glm OpenStage/OpenStageInputService/om_sdk ViconDataStream/ViconDataStreamInputService/ViconLib mpm/swig_mpm_in mpm/swig_mpm_out_csharp mpm/swig_mpm_out_python odl/swig_odl_in odl/swig_odl_out_csharp odl/swig_odl_out_python ProComp2/ProComp2InputService/ProCompLib/inc
+sloccount KinectV2/KinectV2InputService/KinectLib LeapMotion/LeapMotionInputService/Leap*.h NatNet/NatNetInputService/NatNetSDK OpenStage/OpenStageInputService/glm OpenStage/OpenStageInputService/om_sdk ViconDataStream/ViconDataStreamInputService/ViconLib m+m/swig_m+m_in m+m/swig_m+m_out_csharp m+m/swig_m+m_out_python odl/swig_odl_in odl/swig_odl_out_csharp odl/swig_odl_out_python ProComp2/ProComp2InputService/ProCompLib/inc
