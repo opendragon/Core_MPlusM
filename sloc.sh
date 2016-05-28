62311a8e45dc13e4826051e6372dd592ef06f7aa
Norman Jaffe <turing@shaw.ca>
2016-05-28 09:05:32 -0700

    Changed executables attribution, corrected SLOC calculation.

--- sloc.sh
+++ sloc.sh
@@ -4,4 +4,4 @@ sloccount Address Blob ClientList CommonLisp CommonTests FindServices JavaScript
 echo ---------------
 echo subtract the following -
 echo ---------------
-sloccount KinectV2/KinectV2InputService/KinectLib LeapMotion/LeapMotionInputService/Leap*.h NatNet/NatNetInputService/NatNetSDK OpenStage/OpenStageInputService/glm OpenStage/OpenStageInputService/om_sdk ViconDataStream/ViconDataStreamInputService/ViconLib m+m/swig_m+m_in m+m/swig_m+m_out_csharp m+m/swig_m+m_out_python odl/swig_odl_in odl/swig_odl_out_csharp odl/swig_odl_out_python ProComp2/ProComp2InputService/ProCompLib/inc examples/PlaybackFromJSONService/rapidjson
+sloccount KinectV2/KinectV2InputService/KinectLib LeapMotion/LeapDisplayOutputService/JuceLibraryCode LeapMotion/LeapMotionInputService/Leap*.h NatNet/NatNetInputService/NatNetSDK OpenStage/OpenStageInputService/glm OpenStage/OpenStageInputService/om_sdk ViconDataStream/ViconDataStreamInputService/ViconLib m+m/swig_m+m_in m+m/swig_m+m_out_csharp m+m/swig_m+m_out_python odl/swig_odl_in odl/swig_odl_out_csharp odl/swig_odl_out_python ProComp2/ProComp2InputService/ProCompLib/inc examples/PlaybackFromJSONService/rapidjson
